33d166fad4d2fae50a47448f4feebe6820fbca8b
Crispelo <97807364+Crispel0@users.noreply.github.com>
2022-11-13 15:41:22 -0500

    filter compuests

--- filter dates.sql
+++ filter dates.sql
@@ -1,2 +1,6 @@
 select * from tbcliente where year(fecha_nacimiento) = 1992; select * from tbcliente where month(fecha_nacimiento) = 1;
 select * from tbcliente where day(fecha_nacimiento) = 20;
+select * from tbproducto;
+
+#filtro compuesto
+select * from tbproducto where (envase = 'botella de vidrio' and tamano = '700ml') or (precio_lista = 7 or nombre = 'light');
