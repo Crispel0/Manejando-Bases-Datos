cd24a0e69b8e5895566cc63147c2d528c72bbec2
Crispelo <97807364+Crispel0@users.noreply.github.com>
2022-11-13 15:04:47 -0500

    Using Filters, Limit, and Alias(as)

---
+++ As, Limit, Filters.sql
@@ -0,0 +1,3 @@
+select * from tbcliente;
+#As es un alias que se le coloca a los campos de la tabla
+select nombre as nombrecompleto, direccion1 as ubicacion, barrio as localidad, cuidad from tbcliente;
