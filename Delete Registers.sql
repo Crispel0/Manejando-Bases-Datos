718c22e2850bed3a1d5a62d13e085319f1bf6e4b
Crispelo <97807364+Crispel0@users.noreply.github.com>
2022-11-13 12:45:35 -0500

    add primary key

--- Delete Registers.sql
+++ Delete Registers.sql
@@ -1,2 +1,4 @@
 SELECT * FROM tbproductos; /* Important put the where in the moment of delete a register , The drop is for tables a BD normally (Objects) delete for register*/
 DELETE FROM tbproductos WHERE producto = '1022450';
+
+ALTER TABLE tbproductos ADD PRIMARY KEY(producto);
