34ce448de17cee89c129770b3ab00c39717658e4
Crispelo <97807364+Crispel0@users.noreply.github.com>
2022-11-13 23:30:23 -0500

    using filtet and, or ,in , not

---
+++ Filter and,or,not,in.sql
@@ -0,0 +1,11 @@
+use ventas_jugos;
+select * from tb_cliente;
+
+select * from tb_cliente where barrio = 'el carrino' and edad = 26; 
+select * from tb_cliente where not(barrio = 'carola') or edad = 26;
+#filtro compuesto no trae el barrio el carrino o donde el año es 1994
+select * from tb_cliente where not(barrio = 'el carrino' or year (fecha_nacimiento) = 1994);
+
+#these're statements is the same 9 and 10
+select * from tb_cliente where direccion in ('los alagos','condensa');
+select * from tb_cliente where direccion = 'los alagos' or 'condensa';
--- filter with major that and menor that.sql
+++ filter with major that and menor that.sql
@@ -1,4 +1,5 @@
 select * from tbcliente where edad > 20;
 select * from tbcliente where edad < 28;
 select * from tbcliente where edad <> 24;
-
+select precio_lista from tbproducto;
+select * from tbproducto where precio_lista between 6.30 and 6.32;
